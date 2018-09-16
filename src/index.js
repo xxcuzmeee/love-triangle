2164ed0d3093e4a2f4379a72a306c55dc8dea699
xxcuzmeee <veranika.taukachova@gmail.com>
2018-09-16 14:51:44 +0300

    love triangles counted

--- src/index.js
+++ src/index.js
@@ -3,21 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
 
-function getLoveTrianglesCount(){
   let TrianglesNumber, x, y, z;
   TrianglesNumber=0;
-  for (let i = 0; i < preferences.length; i++){
+
+  for ( let i = 0; i < preferences.length; i++ ){
       x = preferences[i]-1;
       y = preferences[x]-1;
       z = preferences[y]-1;
-      if ((z === i) && (x !== y) && (y !== z ) &&  (x !== z)) {
+      if (( z === i ) && ( x !== y ) && ( y !== z ) &&  ( x !== z )) {
           TrianglesNumber++;
       }
   }
-  
+
   if (TrianglesNumber > 0) {
      TrianglesNumber = TrianglesNumber / 3;
   }
