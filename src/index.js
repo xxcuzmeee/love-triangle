b199d192e3b3b121313c0e3be87b4d6cd20495a4
xxcuzmeee <veranika.taukachova@gmail.com>
2018-09-16 14:06:42 +0300

    git commit message love triangles counted

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
 };
+
+function getLoveTrianglesCount(){
+  let TrianglesNumber, x, y, z;
+  TrianglesNumber=0;
+  for (let i = 0; i < preferences.length; i++){
+      x = preferences[i]-1;
+      y = preferences[x]-1;
+      z = preferences[y]-1;
+      if ((z === i) && (x !== y) && (y !== z ) &&  (x !== z)) {
+          TrianglesNumber++;
+      }
+  }
+  if (TrianglesNumber > 0) {
+     TrianglesNumber = TrianglesNumber / 3;
+  }
+  return TrianglesNumber;
+};
